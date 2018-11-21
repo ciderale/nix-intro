54b14b25d453792bb74315c6d79e2e78b35720d2
Alain Lehmann <alain.lehmann@gmail.com>
2018-11-21 23:29:02 +0100

    use fetchTarball to save space and for simplicity https://github.com/NixOS/nix/issues/2431

--- nixpkgs.pinned.nix
+++ nixpkgs.pinned.nix
@@ -1,10 +1,10 @@
 # https://nixos.wiki/wiki/FAQ/Pinning_Nixpkgs
-import (builtins.fetchGit {
-  # Descriptive name to make the store path easier to identify
-  name = "nixos-darwin-2018-11-21";
-  url = https://github.com/nixos/nixpkgs/;
-  # Commit hash for nixos-unstable as of 2018-11-19
-  # `git ls-remote https://github.com/nixos/nixpkgs-channels nixos-unstable`
-  rev = "03dc6471c12a0ef505ad8136ce07d47b332f2def"; # nixos-18.09 as of 2018-11-09
-  ref = "nixos-18.09"; # https://github.com/NixOS/nix/issues/2431
+# https://github.com/NixOS/nix/issues/2431
+# use fetchTarball since simpler and it uses less disk space
+let
+  # `git ls-remote https://github.com/nixos/nixpkgs-channels nixos-18.09`
+  rev = "03dc6471c12a0ef505ad8136ce07d47b332f2def"; # nixos-18.09 as of 2018-11-21
+in import (builtins.fetchTarball {
+  name = "nixos-18.09-2018-11-21"; # Descriptive name to make the store path easier to identify
+  url = "https://github.com/nixos/nixpkgs-channels/archive/${rev}.tar.gz";
 })

8c089f6b3fbcb669fe31699db356bd84d2d3eb60
Alain Lehmann <alain.lehmann@gmail.com>
2018-11-22 22:07:05 +0100

    add comment for awesome-web

--- docker.nix
+++ docker.nix
@@ -1,2 +1,2 @@
 let pkgs = import ./packages { system = "x86_64-linux"; };
-in { inherit (pkgs) awesome-docker; } # awesome-web; }
+in { inherit (pkgs) awesome-docker; } # awesome-web; } # use nixos18.09 instead of unstable
